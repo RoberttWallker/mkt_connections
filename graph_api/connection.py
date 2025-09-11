c739b6998040ad3cc6fe748eb5faa43b658d5734
RoberttWallker <robertwalker.rodrigues@gmail.com>
2025-09-10 21:19:32 -0300

    Arquivos pycache retirados

--- graph_api/connection.py
+++ graph_api/connection.py
@@ -16,7 +16,7 @@ today = datetime.now()
 validity = today + timedelta(days=45)
 
 ACCOUNT_ID = os.getenv("ACCOUNT_ID")
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN_FACEBOOK")
 
 APP_ID = os.getenv("APP_ID")
 APP_SECRET = os.getenv("APP_SECRET")
